ee5665e022092a5ea5bc4a1c7a77ebb60ddd8f8a
Jardes Souza <jardessouza@gmail.com>
2023-03-08 17:43:09 -0300

    Initial settings

---
+++ src/test/java/br/com/jardessouza/evaluatecreditcard/EvaluatecreditcardApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jardessouza.evaluatecreditcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluatecreditcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
